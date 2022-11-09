8ae670d05114b230cf10a193bb054d2483385904
Lorenzo Castiglion <lorenzo.castiglion@hotmail.it>
2022-11-09 15:09:32 +0100

    esercitazione finita con bonus

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+
+
+  <AppTitle />
+
+
+
+
+</template>
+
+<script>
+import AppTitle from './AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle
+  },
+
+};
+</script>
+
+<style lang="css" >
+
+</style>
---
+++ src/AppTitle.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+
+</template>
+
+<script>
+export default {
+    name: 'AppTitle',
+    data() {
+        return {
+            title: 'My first Vite App!'
+        }
+    }
+}
+</script>
+
+<style lang="css">
+header {
+    width: 100%;
+    height: 70px;
+    background-color: black;
+    padding: 20px;
+    color: white;
+    margin: auto;
+    text-align: center;
+}
+</style>
